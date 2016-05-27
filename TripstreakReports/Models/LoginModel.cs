ffebf8e70f229f541dec791b65823ecef85b6a2a
abhishekb <abhishekb@tavisca.com>
2016-05-27 17:15:13 +0530

    Login Page added.

---
+++ TripstreakReports/Models/LoginModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace TripstreakReports.Models
+{
+    public class LoginModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        public string Message { get; set; }
+    }
+}
